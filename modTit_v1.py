27ce1672732796d3ce4621cdf8f1f28e05b00dbe
Rafael Ferreira <ferreiradrrafael@gmail.com>
2022-02-01 10:05:37 -0300

    Fiz previsao com pipeline

--- modTit_v1.py
+++ modTit_v1.py
@@ -10,7 +10,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, mean_absolute_error
-#from sklearn.impute import SimpleImputer
+from sklearn.impute import SimpleImputer
 from scipy.stats import mode
 from sklearn.preprocessing import OneHotEncoder
 
@@ -26,29 +26,6 @@ cat = [col for col in orig_X if orig_X[col].dtype == 'object']
 categoricas = orig_X[cat]
 numericas = orig_X.drop(cat, axis = 1)
 
-categoricas['Embarked'].fillna(method = 'ffill', inplace = True)
-
-    # Optei por remover a idade, pq preenchendo os NULOS sempre desequilibrava demais
-numericas.drop(['Age'], axis = 1, inplace = True)
 
 
-enc = OneHotEncoder(drop = 'first')
-
-OH_X_treino = pd.DataFrame(enc.fit_transform(X_treino[cat]))
-OH_X_treino.index = X_treino.index
-
-
-# Juntando os dois dataFrames: categoricas + numericas
-X = categoricas.join(numericas)
-
-
-    #Split
-X_treino, X_valid, y_treino, y_valid = train_test_split(X, y, train_size = 0.7, random_state = 0)
-
-rf_model = RandomForestClassifier(max_leaf_nodes = 550,
-                                 n_estimators = 1000,
-                                 max_depth = 30)
-rf_model.fit(X_treino, y_treino)
-
-pred = rf_model.predict(X_teste)
 
